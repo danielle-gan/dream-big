bb0a7c0e5bab40b24cdd19c7cb143545c80693d9
danielle-gan <danielle.gan@uconn.edu>
2022-05-03 20:28:50 -0400

    schedule updated for real this time

--- client/src/pages/Schedule.js
+++ client/src/pages/Schedule.js
@@ -8,18 +8,120 @@ import {
   Jumbotron,
   Container,
   Col,
+  Row,
   Form,
   Button,
   Card,
-  CardColumns,
+  CardColumns
 } from "react-bootstrap";
 
 const Schedule = () => {
-  return (
-    <div>
-      <p>Hello</p>
-    </div>
-  );
-};
+    return (
+        <div>
+            <Container>
+                <Button> Add New Task </Button>
+            </Container>
+            <Container>
+                <Row>
+                    <Col>
+                        <p>7 AM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>8 AM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>9 AM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>10 AM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>11 AM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>12 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>1 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>2 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>3 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>4 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>5 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>6 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+            </Container>
+
+        </div >
+    )
+}
 
 export default Schedule;

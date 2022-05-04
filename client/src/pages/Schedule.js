fc5ea587616ece3b16f5fe8ecbf14b482e6a748b
danielle-gan <danielle.gan@uconn.edu>
2022-05-03 20:25:23 -0400

    working on schedule.js needs to be changed

--- client/src/pages/Schedule.js
+++ client/src/pages/Schedule.js
@@ -4,14 +4,115 @@
 //  SORT BY DATE AND START TIME
 
 import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { Jumbotron, Container, Col, Row, Form, Button, Card, CardColumns, Modal } from 'react-bootstrap';
 
 const Schedule = () => {
 
     return (
         <div>
-            <p>Hello</p>
-        </div>
+            <Container>
+                <Button> Add New Task </Button>
+            </Container>
+            <Container>
+                <Row>
+                    <Col>
+                        <p>7 AM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>8 AM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>9 AM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>10 AM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>11 AM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>12 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>1 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>2 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>3 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>4 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>5 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>6 PM</p>
+                    </Col>
+                    <Col>
+                        <textarea></textarea>
+                    </Col>
+                </Row>
+            </Container>
+
+        </div >
     )
 }
 
